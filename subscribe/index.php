d35167bd89448e6704d398cb1f89f889dbb74f77
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2017-12-18 22:03:03 -0300

    Add favicon

--- subscribe/index.php
+++ subscribe/index.php
@@ -12,6 +12,7 @@
   <meta property="og:image:type" content="image/jpeg">
   <meta property="og:image:width" content="1200">
   <meta property="og:image:height" content="630">
+  <link rel="icon" href="<?= SITE_URL ?>/favicon.ico" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   <link href="https://use.fontawesome.com/releases/v5.0.1/css/all.css" rel="stylesheet">
   <link rel="stylesheet" href="./assets/styles/index.css">
